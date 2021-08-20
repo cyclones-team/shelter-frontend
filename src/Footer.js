bb88fdca5222dc97d1f0ddb36b15a2734a13946d
walaamohammad <layan_abadi86@yahoo.com>
2021-08-20 11:47:10 +0300

    css footer and header

--- src/Footer.js
+++ src/Footer.js
@@ -2,44 +2,38 @@ import React, { Component } from 'react'
 import './footer.css';
 
 class Footer extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+<div class="footer">
+  <div class="inner-footer">
+        <ul>
+          <li><i  aria-hidden="true"></i>Renad Walaa Thaer Anas</li>
+          <li><i  aria-hidden="true"></i>  078/43456789  </li>
+          <li><i aria-hidden="true"></i>Cyclones1@gmail.com</li>
+        </ul> 
+       </div>
 
-            <div class="footer">
-            <div class="inner-footer">
-          
-      
-              <div class="footer-items1">
-                <h1>Company Name</h1>
-                <p>Cyclones1 To save shelter Animals.</p>
-              </div>
-          
-              <div class="footer-items2">
-                <h3>Contact us</h3>
-                <div class="border1"></div>
-                  <ul>
-                    <li><i class="fa fa-map-marker" aria-hidden="true"></i>AMMan - jordan</li>
-                    <li><i class="fa fa-phone" aria-hidden="true"></i>078/456789</li>
-                    <li><i class="fa fa-envelope" aria-hidden="true"></i>Cyclones1@gmail.com</li>
-                  </ul> 
-                
-                  <div class="social-media">
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-facebook"></i></a>
-                    <a href="#"><i class="fab fa-google-plus-square"></i></a>
-                  </div> 
-              </div>
-            </div>
-            
-      
-            <div class="footer-bottom">
-              Copyright &copy; Cyclones1 2020.
-            </div>
-          </div>
-                
-           
-        )
-    }
+        <div class="social-media">
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-google-plus-square"></i></a>
+        </div> 
+   
+  </div>
+  <div class="footer-bottom">
+    Copyright &copy; cyclones1 2020.
+  </div>
+</div>
+  
+
+   
+
+  
+
+
+    )
+  }
 }
 
 export default Footer
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './footer.css';
-class Header extends Component {
-    render() {
-        return (
+import './header.css';
 
-<div>
-<div class="header-items">
-                <div class="border1"></div> 
-                  <ul>
-                    <a href="#"><li>Home</li></a>
-                    <a href="#"><li>Profile</li></a>
-                    <a href="#"><li>About us</li></a>
-                    <a href="#"><li>Take Action</li></a>
-                  </ul>
-              </div>
-</div>
-
-        )
-    }
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+         <div class="container"> 
+        <header>
+            <nav>
+                <ul class="menu">
+                    <li class="logo"><a href="#">Dev & Design</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">profile</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Take Action</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="banner">
+            <div>
+                <h2 class="banner-title">Don't Miss Out On Our Next Webinar</h2>
+                <p class="banner-desc">Sign Up Now and Choose an Ebook for Free</p>
+            </div>
+            <button class="btn-signup" type="button" onclick="location.href='#'">
+                   Go to Webinars »
+            </button>
+        </div>
+        <article class="article">
+            <p>...</p>
+        </article>
+    </div>
+      </div>
+    )
+  }
 }
 
 export default Header
