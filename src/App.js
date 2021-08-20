e314ede0d71a3cc321c9d59fef2b9f231165ac20
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-20 17:58:51 +0300

    add AboutUs Component with assets/rhino.png

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/Main";
 import Footer from "./Footer";
 import Header from "./Header";
 
+
 class App extends react.Component {
   render() {
     return (
---
+++ src/components/AboutUs.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import rhino from "./assets/rhino.png";
+import "./aboutUs.css";
+
+export class AboutUs extends Component {
+  render() {
+    return (
+      <>
+        <Container className="mt-5 mb-6">
+          <div class="container">
+            <div class="row flex-center sm-no-flex">
+              <div class="pull-right sm-no-float col-md-8">
+                <ul class="team-members">
+                  <li class="clearfix">
+                    <div class="member-details">
+                      <div>
+                        <img
+                          src="https://via.placeholder.com/100.png/09f/fff"
+                          alt="Web Developer"
+                        />
+                        <div class="member-info">
+                          <h3>Renad Khalafat</h3>
+                          <p>Web Developer</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="member-details">
+                      <div>
+                        <img
+                          src="https://via.placeholder.com/100.png/09f/fff"
+                          alt="Web Developer"
+                        />
+                        <div class="member-info">
+                          <h3>Thaer Jomhawi</h3>
+                          <p>Web Developer</p>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li class="clearfix">
+                    <div class="member-details">
+                      <div>
+                        <img
+                          src="https://via.placeholder.com/100.png/09f/fff"
+                          alt="Web Developer"
+                        />
+                        <div class="member-info">
+                          <h3>Anas AbuSaif</h3>
+                          <p>Web Developer</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="member-details">
+                      <div>
+                        <img
+                          src="https://via.placeholder.com/100.png/09f/fff"
+                          alt="Web Developer"
+                        />
+                        <div class="member-info">
+                          <h3>Walaa Mohammed</h3>
+                          <p>Web Developer</p>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="pull-left col-md-4 sm-text-center">
+                <div class="team-overview">
+                  <h2>Meet The Team</h2>
+                  <p>
+                    <div>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Adipisci quae nesciunt officia! Autem, nam eum voluptatem,
+                      esse voluptatibus deleniti tempora laboriosam sequi
+                      pariatur, explicabo itaque. Fuga, deserunt? Repudiandae,
+                      neque qui.
+                    </div>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+
+       
+          <Container className="" >
+            <Row className="mt-5">
+              <Col></Col>
+              <Col>
+                <h2>Our Vision</h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="ml-5 ">
+                <img src={rhino} alt="rhino" width="400px" />
+              </Col>
+              <Col className="mt-3">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Adipisci quae nesciunt officia! Autem, nam eum voluptatem,
+                  esse voluptatibus deleniti tempora laboriosam sequi pariatur,
+                  explicabo itaque. Fuga, deserunt? Repudiandae, neque qui.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        
+      </>
+    );
+  }
+}
+
+export default AboutUs;
