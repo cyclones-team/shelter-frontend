ecc2008e1e01e665a95bf110539bbef15abe84e3
anas-abusaif <anasabusief@gmail.com>
2021-08-21 12:42:52 +0300

    added authintication

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ import Main from "./components/Main";
 import Footer from "./Footer";
 import Header from "./Header";
 import Charity from "./components/Charity";
-import { Container ,Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import TakeAction from "./components/TakeAction";
 
 
 class App extends react.Component {
   render() {
     return (
-       <>
-       <Header />
-       <Main/>
-    <Footer />
- {/* <TakeAction/>  */}
-   
+      <>
+        <Header />
+        <Main />
+        <Footer />
+
       </>
     );
   }
