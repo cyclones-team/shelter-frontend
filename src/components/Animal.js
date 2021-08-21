ceb7c3581a29948874422a313e3c1e52af224532
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-22 00:50:56 +0300

    ui

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,44 +1,41 @@
 import react from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import axios from 'axios';
+import axios from "axios";
 import { Card, Col } from "react-bootstrap";
 
 class Animal extends react.Component {
   constructor(props) {
     super(props);
     this.state = {
-      arr: [
-        { title: "Lion", description: "King of the jungle" },
-        { title: "tiger", description: "kill mawgli" },
-        { title: "crocodile", description: "hard to kill" },
-        { title: "Lion", description: "King of the jungle" },
-        { title: "tiger", description: "kill mawgli" },
-        { title: "crocodile", description: "hard to kill" },
-        { title: "Lion", description: "King of the jungle" },
-        { title: "tiger", description: "kill mawgli" },
-        { title: "crocodile", description: "hard to kill" },
-        { title: "Lion", description: "King of the jungle" },
-        { title: "tiger", description: "kill mawgli" },
-        { title: "crocodile", description: "hard to kill" },
-      ],
+      endangeredData: [],
     };
   }
+  componentDidMount = async () => {
+    let url = "http://localhost:3020/endangered";
+    console.log(url);
+    axios(url)
+      .then((axiosResults) => {
+        console.log("i am working");
+        console.log(axiosResults);
+
+        this.setState({ endangeredData: axiosResults.data[0].endangeredAnimals });
+      })
+      .catch((err) => console.error(err));
+  };
 
   render() {
     return (
       <>
-        {this.state.arr.map((element) => {
+        {this.state.endangeredData.map((element) => {
           return (
             <>
               <Col className="mb-3">
                 <Card style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src="https://via.placeholder.com/200.png/09f/fff"
-                  />
+                  <Card.Img variant="top" src={element.image_url} />
                   <Card.Body>
-                    <Card.Title>{element.title}</Card.Title>
-                    <Card.Text>{element.description}</Card.Text>
+                    <Card.Title>{element.name}</Card.Title>
+                    <Card.Text>Home: {element.home}</Card.Text>
+                    <Card.Text>Description : {element.description}</Card.Text>
                   </Card.Body>
                 </Card>
               </Col>
