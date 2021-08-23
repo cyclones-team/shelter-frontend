1b343d284b4314c5d917032fb1085ec6e6e02d2b
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-23 18:49:33 +0300

    css the main

--- src/components/Dogfact.js
+++ src/components/Dogfact.js
@@ -1,42 +1,37 @@
 import react from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Carousel } from "react-bootstrap";
-import dogFact from './assets/dog.json';
-
-
+import { Container, Card, Row, Col, Carousel } from "react-bootstrap";
+import dogFact from "./assets/dog.json";
+import "./dogFact.css"
 
 class Dogfact extends react.Component {
   constructor(props) {
     super(props);
     this.state = {
-       dogFact:[]
-        
+      dogFact: [],
     };
   }
-  
-
 
   render() {
     return (
       <>
-        <Carousel style={{ width: "83%", margin: "auto" }}>
-            {dogFact.map((fact,index)=>{
-                return(
-                    <Carousel.Item key={index}>
-                    <img
-                      className="d-block w-100"
-                      src={fact.image}
-                      alt="First slide"
-                    />
-                    <Carousel.Caption>
-                  
-                      <p style={{fontSize:'25px',fontWeight:'600',color:'white'}}>{fact.fact}</p>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-
-                )
-            })}
+      <Container className="justify-content-center">
+        <Carousel  className="justify-content-center" style={{ width: "30rem",margin:"auto" }}>
+          {dogFact.map((fact, index) => {
+            return (
+              <Carousel.Item key={index}>
+                <Card ClassName="shadow rounded border "  style={{ width: "30rem"}}>
+                  <Card.Img  variant="top" src={fact.image}  />
+                  <Card.Body>
+                    <Card.Title>Wof! Wof!</Card.Title>
+                    <Card.Text>{fact.fact}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+            );
+          })}
         </Carousel>
+        </Container>
       </>
     );
   }
